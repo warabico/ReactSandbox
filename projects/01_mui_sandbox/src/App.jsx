4b05dcd5b390c2ebc6b1ab272c149472e3546186
warabico <121906002+warabico@users.noreply.github.com>
2025-01-03 13:35:40 +0900

    [Mody]Layout Text

--- projects/01_mui_sandbox/src/App.jsx
+++ projects/01_mui_sandbox/src/App.jsx
@@ -1,6 +1,10 @@
 import "./App.css";
+import * as React from "react";
 import { Suspense, useState, lazy } from "react";
 import BaseErrorBoundary from "./BaseErrorBoundary";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid2";
 
 const DynamicLoader = ({ labId }) => {
     const LazyComponent = lazy(() => import(`./${labId}/Lab`));
@@ -20,23 +24,27 @@ const App = ({ labs }) => {
     const handleChange = (event) => setLabIndex(event.target.value);
 
     return (
-        <div className="App">
-            <select
-                className="Lab-select"
-                value={labIndex}
-                onChange={handleChange}
-            >
-                {labs.map((id, index) => (
-                    <option key={id} value={index}>
-                        {id}
-                    </option>
-                ))}
-            </select>
-
-            <div className="Lab">
-                <DynamicLoader labId={labId} />
-            </div>
-        </div>
+        <React.Fragment>
+            <CssBaseline />
+            <Grid container spacing={1} margin={1}>
+                <Grid size={4}>
+                    <select
+                        className="Lab-select"
+                        value={labIndex}
+                        onChange={handleChange}
+                    >
+                        {labs.map((id, index) => (
+                            <option key={id} value={index}>
+                                {id}
+                            </option>
+                        ))}
+                    </select>
+                </Grid>
+                <Grid size={8} style={{ border: "dashed 1px lightblue" }}>
+                    <DynamicLoader labId={labId} />
+                </Grid>
+            </Grid>
+        </React.Fragment>
     );
 };
 
--- projects/01_mui_sandbox/src/labs.js
+++ projects/01_mui_sandbox/src/labs.js
@@ -1,3 +1,3 @@
-const labs = ["010_button"];
+const labs = ["010_button", "020_layout"];
 
 export default labs;
