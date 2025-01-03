705202ba2020fcbb3d96c78c229c4282350c04ed
warabico <121906002+warabico@users.noreply.github.com>
2025-01-03 15:45:22 +0900

    [Modify]Local data storage lab

--- projects/01_mui_sandbox/src/labs.js
+++ projects/01_mui_sandbox/src/labs.js
@@ -1,3 +1,3 @@
-const labs = ["010_button", "020_layout", "030_card"];
+const labs = ["010_button", "020_layout", "030_card", "040_local_data"];
 
 export default labs;
---
+++ projects/01_mui_sandbox/src/labs/040_local_data/Lab.jsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+
+const STORAGE_KEY = "local-storage-lab-data-2";
+
+const Lab = () => {
+    let restoredLabData = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? [
+        {
+            username: "John",
+            rating: 1582,
+            history: ["win", "lose", "win"],
+        },
+    ];
+
+    const [labData, setLabData] = React.useState(restoredLabData);
+
+    const addLabData = () => {
+        let newLabData = [...labData];
+        newLabData.push({
+            username: "Tom",
+            rating: Math.floor(Math.random() * 3e3),
+            history: ["win", "lose"],
+        });
+        setLabData(newLabData);
+    };
+
+    const delLabData = () => {
+        let newLabData = [...labData];
+        newLabData.pop();
+        setLabData(newLabData);
+    };
+
+    const resetData = () => {
+        localStorage.removeItem(STORAGE_KEY);
+        setLabData([]);
+    };
+
+    React.useEffect(() => {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(labData));
+    });
+
+    return (
+        <>
+            <div>
+                {labData.map((data) => {
+                    return (
+                        <div key={data.rating}>
+                            <p>{data.username}</p>
+                            <p>{data.rating}</p>
+                            <p>{data.history}</p>
+                            <hr />
+                        </div>
+                    );
+                })}
+            </div>
+            <button onClick={addLabData}>add</button>
+            <button onClick={delLabData}>del</button>
+            <button onClick={resetData}>reset</button>
+        </>
+    );
+};
+
+export default Lab;
